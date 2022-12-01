e35e7f7d727ce93ea607d5e4e43836c46f28ea12
kevin.mutua@student.moringaschool.com <kevin.mutua@student.moringaschool.com>
2022-12-01 07:38:37 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+const record = [
+  { year: "2018", result: "L" },
+  { year: "2015", result: "W" },
+  { year: "2016", result: "N/A" }
+  //...
+]
+let year;
+//data from indexTest.js 
+//need to define a fuction superbowlWin that takes 1 argument, an `Array` of JavaScript `Object(record)
+
+const superbowlWin = record => {
+  
+  const result = record.find(record => record.result === "W");
+  if(result)
+      return result.year;
+
+  
+}
+
+console.log(superbowlWin(record));
